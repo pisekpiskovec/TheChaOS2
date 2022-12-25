ad1562d1305058485e833978954a7df5faad37b1
Písek Pískovec <pisekpiskovec@gmail.com>
2022-12-25 21:43:40 +0100

    Please dont show you bot's token =)
    and if you do, dont forget to reset it later

--- main.js
+++ main.js
@@ -1,54 +1,68 @@
-const { Client, GatewayIntentBits, EmbedBuilder, PermissionsBitField, Permissions, Message, messageLink } = require("discord.js");
+const { Client, ActivityType } = require("discord.js");
+require("dotenv").config();
 
-const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
+const client = new Client({
+  intents: ["Guilds"],
+});
 
-var rarities = [{
+var rarities = [
+  {
     msg: "Welcome aboard Captain, all systems online.",
-    chance: 0
-},
-{
+    chance: 0,
+  },
+  {
     msg: "Welcome aboard Captain, some systems need attention.",
-    chance: 20
-},
-{
+    chance: 20,
+  },
+  {
     msg: "Engine powering up...",
-    chance: 35
-},
-{
+    chance: 35,
+  },
+  {
     msg: "You are the best captain on the planet, I'm not even squiddin'.",
-    chance: 10
-},
-{
+    chance: 10,
+  },
+  {
     msg: "I love it when you come inside me.",
-    chance: 5
-
-}]
+    chance: 5,
+  },
+];
 
 function startMessage() {
-    var filler = 100 - rarities.map(r => r.chance).reduce((sum, current) => sum + current);
-    if (filler <= 0) {
-        console.log("chances sum is higher than 100!");
-        return;
-    }
-    var probability = rarities.map((r, i) => Array(r.chance === 0 ? filler : r.chance).fill(i)).reduce((c, v) => c.concat(v), []);
-    var pIndex = Math.floor(Math.random() * 100);
-    var rarity = rarities[probability[pIndex]];
+  var filler =
+    100 - rarities.map((r) => r.chance).reduce((sum, current) => sum + current);
+  if (filler <= 0) {
+    console.log("chances sum is higher than 100!");
+    return;
+  }
+  var probability = rarities
+    .map((r, i) => Array(r.chance === 0 ? filler : r.chance).fill(i))
+    .reduce((c, v) => c.concat(v), []);
+  var pIndex = Math.floor(Math.random() * 100);
+  var rarity = rarities[probability[pIndex]];
 
-    return (rarity.msg);
+  return rarity.msg;
 }
+client.on("messageCreate", (message) => {
+  if (message.content == "|m") {
+    message.channel.send("🗿");
+  }
+  if (message.content == "stfu bitch") {
+    message.channel.send("no");
+  }
+});
 
-client.on("ready", () => {
+client
+  .login(process.env.TOKEN)
+  .then(() => {
     console.log(`Cyclops: ${startMessage()}`);
-
     client.user.setAvatar("./resources/alpha1.jpg");
     client.user.setUsername("The ChaOS 2nd Generation");
-    client.user.setActivity("Pisek", { type: "WATCHING" });
-})
-client.on("messageCreate", (message) => {
-    if(message.content == "|m"){message.channel.send("🗿");}
-    if(message.content == "stfu bitch"){message.channel.send("no");}
-})
-
-
-
-client.login("OTQ3NTgyMjI3MDcwNDY0MTEx.GtxTPC.9mW4PXGlLtya6L-OO1WKWxMF8vdim7iCacIBqs");
+    client.user.setActivity("Pisek", { type: ActivityType.Listening });
+    //client.user.setActivity("Pisek", {type: ActivityType.Competing});
+    //client.user.setActivity("Nora", {type: ActivityType.Watching});
+  })
+  .catch((err) => {
+    console.log("Cyclops: Captain, some systems need attention!");
+    console.log(`Cyclops > ${err}`);
+  });
