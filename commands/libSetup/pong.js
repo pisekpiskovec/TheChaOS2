3eb07c1b25daf13be90badd5070157dd51448352
Písek Pískovec <pisekpiskovec@gmail.com>
2022-12-28 18:55:58 +0100

    [Commander] [libSetup] Fixed 'pong' command

--- commands/libSetup/pong.js
+++ commands/libSetup/pong.js
@@ -1,24 +1,19 @@
 const {
     ChatInputCommandInteraction,
     SlashCommandBuilder,
-    Client,
-    GatewayIntentBits
+    Client
 } = require("discord.js");
-const { Guilds, GuildMembers, GuildMessages } = GatewayIntentBits;
-
-const clienter = new Client({
-    intents: [Guilds, GuildMembers, GuildMessages],
-});
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName("pong")
         .setDescription("Will respond with ping."),
     /**
-     *
-     * @param {ChatInputCommandInteraction} interaction
+     * 
+     * @param {ChatInputCommandInteraction} interaction 
+     * @param {Client} client 
      */
-    execute(interaction) {
-        interaction.reply({ content: `Ping: ${clienter.ws.ping} ms` });
+    execute(interaction, client) {
+        interaction.reply({ content: `Ping: ${client.ws.ping} ms` });
     },
 };
