19ac632ddc8e904bdd496a893d0667cba63795c8
Písek Pískovec <pisekpiskovec@gmail.com>
2023-03-13 20:29:51 +0100

    [Commander] [libRole] Clean code

--- commands/libRole/roleToggle.js
+++ commands/libRole/roleToggle.js
@@ -24,35 +24,35 @@ module.exports = {
         if (botSettings.libRole.roleToggleRoles.roleIDs.length < botSettings.libRole.roleToggleRoles.roleNames.length) return interaction.reply({ content: "Number of IDs does not match number of Roles" });
         if (botSettings.libRole.roleToggleRoles.roleIDs.length > botSettings.libRole.roleToggleRoles.roleNames.length) return interaction.reply({ content: "Number of IDs does not match number of Roles" });
 
-        const stringOptionA = interaction.options.getString("role");
+        const stringOption = interaction.options.getString("role");
         const booleanOption = interaction.options.getBoolean("toggle");
 
 
-        var roleID = botSettings.libRole.roleToggleRoles.roleNames.findIndex(v => v.includes(stringOptionA.toString()))
+        var roleID = botSettings.libRole.roleToggleRoles.roleNames.findIndex(v => v.includes(stringOption.toString()))
         switch (booleanOption) {
             case true:
                 interaction.guild.members.cache.get(interaction.member.id).roles.add(botSettings.libRole.roleToggleRoles.roleIDs[roleID]);
-                interaction.reply(`libRole: Added role ${stringOptionA} to you`).then(() => {
-                    console.log(`Commander: Added role ${stringOptionA} to ${interaction.guild.members.cache.get(interaction.member.id).displayName}`)
+                interaction.reply(`libRole: Added role ${stringOption} to you`).then(() => {
+                    console.log(`Commander: Added role ${stringOption} to ${interaction.guild.members.cache.get(interaction.member.id).displayName}`)
                 });
                 break;
             case false:
                 interaction.guild.members.cache.get(interaction.member.id).roles.remove(botSettings.libRole.roleToggleRoles.roleIDs[roleID]);
-                interaction.reply(`libRole: Removed role ${stringOptionA} from you`).then(() => {
-                    console.log(`Commander: Removed role ${stringOptionA} from ${interaction.guild.members.cache.get(interaction.member.id).displayName}`)
+                interaction.reply(`libRole: Removed role ${stringOption} from you`).then(() => {
+                    console.log(`Commander: Removed role ${stringOption} from ${interaction.guild.members.cache.get(interaction.member.id).displayName}`)
                 });
                 break;
             case null:
                 if (interaction.guild.members.cache.get(interaction.member.id).roles.cache.has(botSettings.libRole.roleToggleRoles.roleIDs[roleID])) {
                     interaction.guild.members.cache.get(interaction.member.id).roles.remove(botSettings.libRole.roleToggleRoles.roleIDs[roleID]);
-                    interaction.reply(`libRole: Removed role ${stringOptionA} from you`).then(() => {
-                        console.log(`Commander: Removed role ${stringOptionA} from ${interaction.guild.members.cache.get(interaction.member.id).displayName}`)
+                    interaction.reply(`libRole: Removed role ${stringOption} from you`).then(() => {
+                        console.log(`Commander: Removed role ${stringOption} from ${interaction.guild.members.cache.get(interaction.member.id).displayName}`)
                     });
                 }
                 else {
                     interaction.guild.members.cache.get(interaction.member.id).roles.add(botSettings.libRole.roleToggleRoles.roleIDs[roleID]);
-                    interaction.reply(`libRole: Added role ${stringOptionA} to you`).then(() => {
-                        console.log(`Commander: Added role ${stringOptionA} to ${interaction.guild.members.cache.get(interaction.member.id).displayName}`)
+                    interaction.reply(`libRole: Added role ${stringOption} to you`).then(() => {
+                        console.log(`Commander: Added role ${stringOption} to ${interaction.guild.members.cache.get(interaction.member.id).displayName}`)
                     });
 
                 }
