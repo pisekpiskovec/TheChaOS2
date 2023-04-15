8e079da683a3e3674006f67f1fb856da61a3850b
Písek Pískovec <pisekpiskovec@gmail.com>
2023-04-15 22:15:29 +0200

    [Commandr] [libRoot] Added muting/timeouting

--- commands/libRoot/mute.js
+++ commands/libRoot/mute.js
@@ -19,9 +19,37 @@ module.exports = {
      * 
      * @param {ChatInputCommandInteraction} interaction 
      */
-    execute(interaction) {
+    async execute(interaction) {
         const memberOption = interaction.options.getMember("member");
-        const stringOption = interaction.options.getString("reason");
+        const stringOption = interaction.options.getString("reason") || "No reason provided.";
 
+        await interaction.deferReply();
+
+        const memberMute = interaction.guild.members.fetch(memberOption.id);
+        if (!memberMute) {
+            await interaction.editReply(`[libRoot] User cannot be muted since they doesn't exist in this server.`);
+            return;
+        } else if (memberMute.id === interaction.guild.ownerId) {
+            await interaction.editReply(`[libRoot] User cannot be muted since they are the owner of this server.`);
+            return;
+        }
+        const targetUserRolePosition = (await memberMute).roles.highest.position;
+        const requestUserRolePosition = interaction.member.roles.highest.position;
+        const botRolePosition = interaction.guild.members.me.roles.highest.position;
+
+        if (targetUserRolePosition >= requestUserRolePosition) {
+            await interaction.editReply(`[libRoot] You can't mute the user becuse they are on different level.`);
+            return;
+        }
+        else if (targetUserRolePosition >= botRolePosition) {
+            await interaction.editReply(`[libRoot] Muting this person would cause so much chaos.`);
+            return;
+        }
+        try {
+            await (await memberMute).timeout({ stringOption });
+            await interaction.editReply(`[libRoot] Member succesfuly muted with reason: ${stringOption}  .`);
+        } catch (error) {
+            await interaction.editReply(`[libRoot] An error occured during muting: ${error}`);
+        }
     }
 }
