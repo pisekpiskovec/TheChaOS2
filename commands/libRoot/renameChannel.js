8c2119a4f7ad470fac0f40a29185b5f90f160e30
Písek Pískovec <pisekpiskovec@gmail.com>
2023-04-18 23:05:31 +0200

    [Commander] [libRoot] renameChannel cmd

--- commands/libRoot/renameChannel.js
+++ commands/libRoot/renameChannel.js
@@ -20,9 +20,23 @@ module.exports = {
      * 
      * @param {ChatInputCommandInteraction} interaction 
      */
-    execute(interaction) {
+    async execute(interaction) {
         const channelOption = interaction.options.getChannel("channel");
         const stringOption = interaction.options.getString("new_name");
+
+        await interaction.deferReply();
+
+        const channelRename = interaction.guild.channels.fetch(channelOption.id)
+        if (!channelRename) {
+            await interaction.editReply(`[libRoot] Channel cannot be renamed since they doesn't exist in this server.`);
+            return;
+        }
+        try {
+            await (await channelRename).setName(stringOption);
+            await interaction.editReply(`[libRoot] Channel succesfuly renamed to: **"${stringOption}"**.`);
+        } catch (error) {
+            await interaction.editReply(`[libRoot] An error occured during renaming: ${error}`);
+        }
 
     }
 }
